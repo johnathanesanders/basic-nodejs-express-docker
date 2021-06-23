5ad88867f8a066dacc9ae769448eae95ffe7210e
Johnathan Sander <Johnathan@Johnathons-MBP.attlocal.net>
2021-06-23 09:06:19 -0500

    Cleanup of initial commit

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,4 @@ RUN apk update && apk add libstdc++ && apk add build-base && apk add python3 && 
 COPY . .
 RUN npm install 
 COPY . .
-#EXPOSE 8800
 CMD ["npm", "run", "start:prod"]
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const { Environment } = require('./environment.js');
 const express = require('express');
 
 const server = express();
+const port = Environment.port || 3000;
 
 server.get('/', (request, response) => {
     response.send('Hello World!');
 });
   
-server.listen(Environment.port || 3000, () => {
-    console.log(`Server UP on port ${Environment.port}`);
+server.listen(port, () => {
+    console.log(`Server UP on port ${port}`);
 });
